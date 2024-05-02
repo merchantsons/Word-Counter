4aa8b49767bb60eda885db3927946693d2f3f62f
merchantsons <merchantsons2@gmail.com>
2024-05-02 03:32:18 -0700

    FINAL COMMIT

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 #! /usr/bin/env node
-
 import inquirer from "inquirer";
 import chalk from "chalk";
-
 console.log(chalk.bold.green("\n \t   Word Counter Project  -  By Merchantsons"));
 console.log(chalk.red("♥".repeat(60)));
-
 //inquirer input
-let answers = await inquirer.prompt({name : "setofword", type : "input", message: "Enter Set Of Words Here: →",});
-
+let answers = await inquirer.prompt({ name: "setofword", type: "input", message: "Enter Set Of Words Here: →", });
 //Get input in array with split and trim function.
 let words = answers.setofword.trim().split(" ");
-
 //Check & Print Words
 console.log("☼".repeat(60));
 console.log(chalk.bold("- Words In Array:"));
